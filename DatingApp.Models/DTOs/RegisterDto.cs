deb71ba4ea0118ee322a48e83cad0a1d921f077a
JuanOrjuelaA <jorjuela@lean-tech.io>
2020-10-26 11:32:13 -0500

    Add field validations to avoid empty values

--- DatingApp.Models/DTOs/RegisterDto.cs
+++ DatingApp.Models/DTOs/RegisterDto.cs
@@ -1,9 +1,19 @@
 ﻿namespace DatingApp.Models.DTOs
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class RegisterDto
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required]
         public string UserName { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        [Required]
         public string Password { get; set; }
     }
 }
